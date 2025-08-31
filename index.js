ca73c6fc623a543283057f6e31f5c4743f87c50a
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-08-30 20:58:08 -0700

    makeing it move and api

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-x = 100;
-y = 200;
+const ISS = document.getElementById('ISS');
+
+function MoveAndGetISS() {
+  fetch("https://api.wheretheiss.at/v1/satellites/25544")
+    .then(response => response.json())
+    .then(data => {
+      let lat = data.latitude;    
+      let lon = data.longitude;   
+
+      
+      let x = (lon + 180) * (window.innerWidth / 360);
+      let y = (90 - lat) * (window.innerHeight / 180);
+
+      
+      ISS.style.position = "absolute";
+      ISS.style.left = x + "px";
+      ISS.style.top = y + "px";
+    });
+}
+
+// run every 2 seconds
+setInterval(MoveAndGetISS, 100);
